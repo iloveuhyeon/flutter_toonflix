7a9ff4609d15d3579001b9b4f0030da35db75566
iloveuhyeon <pyh1111802@naver.com>
2023-11-16 14:55:59 +0900

    statefulwidget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,39 +12,70 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<int> numbers = [];
-  void onClick() {
+  bool showtitle = true;
+
+  void channgetitle() {
     setState(() {
-      numbers.add(numbers.length);
+      showtitle = !showtitle;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(fontSize: 30),
-              ),
-              for (var n in numbers)
-                Text(
-                  '$n',
-                  style: const TextStyle(fontSize: 30),
-                ),
-              IconButton(
-                onPressed: onClick,
-                icon: const Icon(Icons.add_box),
-                iconSize: 50,
-              ),
+              showtitle ? const MyLargeTitle() : const Text('notion'),
+              IconButton(onPressed: channgetitle, icon: const Icon(Icons.abc))
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print('hello initSTate');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose!");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Text(
+      '집 가고싶다 ',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
